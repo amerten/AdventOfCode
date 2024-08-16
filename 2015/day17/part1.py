3246c402c25fa5730d0acde0fa833a18c2f09cd2
amerten <adrien.mertens@iba-group.com>
2024-08-16 10:16:23 +0200

    Day 16 2015

---
+++ 2015/day17/part1.py
@@ -0,0 +1,14 @@
+combinations = []
+def search(remaining, used):
+    for i in range(len(remaining)):
+        r, u = remaining[i+1:], used + [remaining[i]]
+        s = sum(u)
+        if s == 150:
+            combinations.append(u)
+        elif s < 150:
+            search(r, u)
+
+
+containers = list(map(int, open(0).readlines()))
+search(containers, [])
+print(len(combinations))
