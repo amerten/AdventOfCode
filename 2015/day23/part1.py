ff94655a6ea169db07d9f73aa0a010334ba17326
amerten <adrien.mertens@iba-group.com>
2024-08-29 11:04:28 +0200

    Day 23 2015

---
+++ 2015/day23/part1.py
@@ -0,0 +1,30 @@
+registers, p = {'a': 0, 'b': 0}, 0
+instructions = open(0).read().splitlines()
+while True:
+    try:
+        instruction = instructions[p].replace(',', '').split()
+        if instruction[0] == "hlf":
+            registers[instruction[1]] //= 2
+            p += 1
+        elif instruction[0] == "tpl":
+            registers[instruction[1]] *= 3
+            p += 1
+        elif instruction[0] == "inc":
+            registers[instruction[1]] += 1
+            p += 1
+        elif instruction[0] == "jmp":
+            p += int(instruction[1])
+        elif instruction[0] == "jie":
+            if registers[instruction[1]] % 2 == 0:
+                p += int(instruction[2])
+            else:
+                p += 1
+        else:
+            if registers[instruction[1]] == 1:
+                p += int(instruction[2])
+            else:
+                p += 1
+    except:
+        break
+
+print(registers["b"])
