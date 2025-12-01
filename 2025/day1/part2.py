18725646bb99cfe079a9c585036419dbe95f3dc0
amerten <adrien.mertens@iba-group.com>
2025-12-01 09:48:24 +0100

    Day 1

---
+++ 2025/day1/part2.py
@@ -0,0 +1,10 @@
+t = 0
+p = 50
+input = open(0).read().splitlines()
+for rot in input:
+    dir, dist = rot[0], int(rot[1:])
+    dir = 1 if dir == 'R' else -1
+    for _ in range(dist):
+        p = (p + dir) % 100
+        t += p == 0
+print(t)
